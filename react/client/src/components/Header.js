83fdade6c62a203b60338f4adfa284702af4ddf5
Yusuf ARIKDOGAN <yusufarikdogann@gmail.com>
2024-03-23 23:45:26 +0300

    update

--- react/client/src/components/Header.js
+++ react/client/src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
       <div className="text-center">
         <img src={cw} alt="" className="cw" />
         <h6 className="text-center mt-5">
-          """"This app has been created by Yusuf Arikdogan on behalf of Konzek for the Junior Level Assignment"
+          "This app has been created by Yusuf Arikdogan on behalf of Konzek for the Junior Level Assignment"
         </h6>
         <h1 className="text-center mt-5 header-text">Junior Level Todos</h1>
       </div>
