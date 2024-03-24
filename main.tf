4cbd76087aaba30eef4271f3067c709f737c6459
Yusuf ARIKDOGAN <yusufarikdogann@gmail.com>
2024-03-25 01:47:49 +0300

    updated

--- main.tf
+++ main.tf
@@ -101,14 +101,14 @@ resource "aws_security_group" "postgresql_security_group" {
   }
 }
 
-output "react_security_group_id" {
-  value = aws_security_group.react_security_group.id
+output "react_ip" {
+  value = "http://${aws_instance.managed_nodes[0].public_ip}:3000"
 }
 
-output "nodejs_security_group_id" {
-  value = aws_security_group.nodejs_security_group.id
+output "node_public_ip" {
+  value = aws_instance.managed_nodes[1].public_ip
 }
 
-output "postgresql_security_group_id" {
-  value = aws_security_group.postgresql_security_group.id
+output "postgre_private_ip" {
+  value = aws_instance.managed_nodes[2].private_ip
 }
